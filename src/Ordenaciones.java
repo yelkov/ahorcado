8d3ff9f90fd2f670da65c1a4e0624ccb59d091f4
a23yelkovq <yelkovq@gmail.com>
2024-09-18 12:55:44 +0200

    feat(Ordenaciones.java): añadir diferentes tipos de nombres archivo

--- src/Ordenaciones.java
+++ src/Ordenaciones.java
@@ -13,14 +13,15 @@ public class Ordenaciones {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa ruta del archivo: ");
         String archivo = sc.nextLine();
-        System.out.println("Ingresa el tipo de ordenación: ");
+        System.out.println("Ingresa el tipo de ordenación\n (A - ascendente case sensitive)\n (a - ascendente case insensitive)\n (D - descendente case sensitive)\n (d - descendente case insensitive)\n: ");
         String tipoOrden = sc.nextLine();
 
         try{
             List<String> lineas = Files.readAllLines(Paths.get(archivo));
             List<String> listaOrdenada = ordenarLineas(lineas, tipoOrden);
+            String nombreArchivo = asignarNombre(archivo, tipoOrden);
 
-            BufferedWriter archivoOrdenado = Files.newBufferedWriter(Paths.get("res/archivoOrdenado.txt"));
+            BufferedWriter archivoOrdenado = Files.newBufferedWriter(Paths.get(nombreArchivo));
 
             for (String linea : listaOrdenada) {
                 archivoOrdenado.write(linea);
@@ -51,6 +52,20 @@ public class Ordenaciones {
                 return lineas.stream().sorted(String.CASE_INSENSITIVE_ORDER.reversed()).toList();
             default:
                 throw new IllegalArgumentException("Tipo de ordenación no válido.");
+            }
         }
-    }
+        public static String asignarNombre(String nombreArchivo, String tipoOrdenacion) throws IllegalArgumentException {
+            switch (tipoOrdenacion) {
+                case "A":
+                    return nombreArchivo + "_asc_case.txt";
+                case "a":
+                    return nombreArchivo + "_asc_non_case.txt";
+                case "D":
+                    return nombreArchivo + "_desc_case.txt";
+                case "d":
+                    return nombreArchivo + "_desc_non_case.txt";
+                default:
+                    throw new IllegalArgumentException("Tipo de ordenación no válido.");
+            }
+        }
 }
