f5b6dfd9e19cc4560b87aea9dd571a00ae1091d9
a23yelkovq <yelkovq@gmail.com>
2024-09-19 10:49:08 +0200

    feat(ListarDirectorioRecursivo.java): implementar recursividad

---
+++ src/ListarDirectorioRecursivo.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class ListarDirectorioRecursivo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce ruta del directorio: ");
+        String nombre = sc.nextLine();
+
+        Path dir = Path.of(nombre);
+        if (Files.isDirectory(dir)) {
+            listarDirectorio(dir,0);
+        } else {
+            System.err.println(dir.toString()+" no es un directorio");
+        }
+    }
+
+    private static void listarDirectorio(Path dir, int nivel) {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir);) {
+            for (Path fichero: stream) {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < nivel; i++){
+                    sb.append("\t");
+                }
+                sb.append(extraerPermisos(fichero));
+                sb.append(" " + fichero.toFile().getName());
+                System.out.println(sb.toString());
+
+                Deque<Path> stack = new ArrayDeque<Path>();
+                if(fichero.toFile().isDirectory()){
+                    stack.push(fichero);
+                }
+
+                while (!stack.isEmpty()) {
+                    Path p = stack.pop();
+                    listarDirectorio(p, nivel+1);
+                }
+            }
+        } catch (IOException | DirectoryIteratorException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    private static StringBuilder extraerPermisos(Path fichero){
+        StringBuilder sb = new StringBuilder();
+        File file = fichero.toFile();
+
+        sb.append(file.isDirectory()? "d" : "-");
+        sb.append(file.canRead()? "r" : "-");
+        sb.append(file.canWrite()? "w" : "-");
+        sb.append(file.canExecute()? "x" : "-");
+
+        return sb;
+    }
+}
