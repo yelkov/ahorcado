7c344742425db1eaa1f24d9c08e5fe56ed5655b5
yelkov <yelkovq@gmail.com>
2024-09-21 17:47:08 +0200

    feat(Minishell.java): implementar cat

--- src/Minishell.java
+++ src/Minishell.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Scanner;
 
 public class Minishell {
@@ -8,12 +11,13 @@ public class Minishell {
         Path directorio = Path.of(directorioActual);
         Scanner sc = new Scanner(System.in);
         Boolean salida = false;
-        System.out.println(">" + directorio.getFileName());
 
         while(!salida){
+            System.out.println(directorio.toAbsolutePath() + ">");
             String input = sc.nextLine();
+            String comando = input.split(" ",2)[0];
 
-            switch(input){
+            switch(comando){
                 case "exit":
                     salida = true;
                     break;
@@ -24,24 +28,51 @@ public class Minishell {
                     ListarDirectorioRecursivo.listarDirectorio(directorio,0);
                     break;
                 case "pwd":
-                    System.out.println(directorio.toAbsolutePath().toString());
+                    System.out.println(directorio.toAbsolutePath().toString() + "\n");
+                    break;
+                case "cd":
+                    directorio = cambiarDirectorio(input, directorio);
+                    break;
+                case "cat":
+                    leerArchivo(input,directorio);
                     break;
                 default:
-                    if (input.startsWith("cd")){
-                        directorio = cambiarDirectorio(input);
-                    }else{
-                        System.out.println("El comando introducido es desconocido");
-                    }
+                        System.out.println('"'+input.toString()+'"' + " no se reconoce como un comando.");
+
             }
         }
     }
 
-    private static Path cambiarDirectorio(String input) {
-        Path directorio;
+    private static Path cambiarDirectorio(String input, Path directioActual) {
         String nuevaRuta = input.replace("cd ","");
-        directorio = Path.of(nuevaRuta);
-        System.out.println(">" + directorio.toAbsolutePath().toString());
-        return directorio;
+        Path nuevoDirectorio = directioActual.resolve(nuevaRuta).normalize();
+
+        if(Files.isDirectory(nuevoDirectorio)){
+            return nuevoDirectorio;
+        }
+        else{
+            System.out.println("El sistema no puede encontrar la ruta especificada.");
+            return directioActual;
+        }
+    }
+
+    private static void leerArchivo(String input, Path directorioActual){
+        String fichero = input.replace("cat ","");
+        Path rutaArchivo = directorioActual.resolve(fichero);
+
+        if (Files.isRegularFile(rutaArchivo)){
+            try{
+                List<String> lineas = Files.readAllLines(rutaArchivo);
+                for (String linea : lineas){
+                    System.out.println(linea);
+                }
+            }
+            catch (IOException e){
+                System.out.println("Error al abrir el archivo.");
+            }
+        }else{
+            System.out.println("El archivo especificado no se encuentra o no puede ser leído.");
+        }
     }
 
 }
